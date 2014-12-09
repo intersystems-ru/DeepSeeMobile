d6b739765c30f2b6fa4488be4ec3848f943ce160
Gnibeda <anton.donnu@gmail.com>
2014-12-09 21:22:10 +0200

    now login screen will appears first

--- www/js/main.js
+++ www/js/main.js
@@ -24,117 +24,46 @@ require([
 ], function (MessageCenter, LoadingSpinner, DBConnector, Dashboard, FiltersView, Utils, IScroll, ViewManager,Utilizer,Filter) {
     
     window.App = {};
-    var login  = "";
-    var pass   = "";
 
-    $("#Login").focus();
-    $("#btnLogin").on("tap", DoLogin);
-    //Start();
+    App.settings = {
+        server: "",
+        namespace: ""
+    };
 
-    function make_base_auth(user, password) {
-        var tok = user + ':' + password;
-        var hash = btoa(tok);
-        return "Basic " + hash;
-    }
-
-    function DoLogin() {
-        $("#txtError").hide();
-        login = $("#txtLogin").val();
-        pass = $("#txtPassword").val();
-        if (login && pass) {
-            $("#loginProgress").show();
-            $.ajax
-            ({
-                type: "GET",
-                url: "http://37.139.4.54/tfomsauth/Test",
-                beforeSend: function (xhr) {
-                    xhr.setRequestHeader("Authorization", make_base_auth(login, pass));
-                },
-                error: loginError,
-                success: loginSuccess
-            });
-        }
-    }
-
-    function loginSuccess(d){
-        $("#txtError").hide();
-        $("#loginProgress").hide();
-        Start();
-    }
-
-    function loginError(d) {
-        $("#loginProgress").hide();
-        $("#txtError").text("Wrong login or password").show();
-    }
-
-    function Start() {
-        $.ajaxPrefilter(function( options ) {
-            if (!options.beforeSend) {
-                options.beforeSend = function (xhr) {
-                    xhr.setRequestHeader('Authorization', make_base_auth(login, pass));
-                    //xhr.setRequestHeader('Authorization', make_base_auth("_SYSTEM", "159eAe72a79539f32acb15b305030060"));
-                }
+    App.m = MessageCenter;
+    App.v = ViewManager;
+    App.filters = [];
+    MessageCenter.subscribe("filters_acquired", {
+        subscriber: this,
+        callback: function (d) {
+            for (var i = 0; i < d.data.data.length; i++) {
+                App.filters.push(new Filter(d.data.data[i], d.data.cube, d.data.widget));
             }
-        });
-
-        $("#loginScreen").remove();
-        $("#mainScreen").show();
-        App.settings = {
-
-            get server(){return (localStorage.settings_server || "")},
-            set server(v){ localStorage.settings_server = v;},
-            get username(){return (localStorage.settings_username || "")},
-            set username(v){ localStorage.settings_username = v;},
-            get password(){return (localStorage.settings_password || "")},
-            set password(v){ localStorage.settings_password = v;},
-            get cubeName(){return (localStorage.settings_cube || "")},
-            set cubeName(v){ localStorage.settings_cube = v;},
-            get namespace(){return (localStorage.settings_namespace || "")},
-            set namespace(v){ localStorage.settings_namespace = v;}
         }
+    });
+    //MessageCenter.publish('filters_requested');
 
-        //set default connection if not exists
-        if (!App.settings.server) App.settings.server = "http://37.139.4.54/tfoms";
-        if (!App.settings.username) App.settings.username = "_SYSTEM";
-        if (!App.settings.password) App.settings.password = "Ae72a79539f32acb15b305030060";
-        if (!App.settings.namespace) App.settings.namespace = "samples";
-        //if (!App.settings.cubeName) App.settings.cubeName = "HoleFoods";
+    MessageCenter.publish("viewchange:Login", {
+        holder: "#loginScreen"
+    });
 
-        App.m = MessageCenter;
-        App.v = ViewManager;
-        App.filters = [];
-        MessageCenter.subscribe("filters_acquired", {
-            subscriber: this,
-            callback: function (d) {
-                for (var i = 0; i < d.data.data.length; i++) {
-                    App.filters.push(new Filter(d.data.data[i], d.data.cube, d.data.widget));
-                }
-            }
-        });
-        //MessageCenter.publish('filters_requested');
-
-        MessageCenter.publish("viewchange:DashboardList", {
+    $("a.nav-home").off('tap').on('tap', function (e) {
+        e.preventDefault();
+        MessageCenter.publish('viewchange:DashboardList', {
             holder: "#mainScreen > .content"
         });
-        $("a.nav-home").off('tap').on('tap', function (e) {
-            e.preventDefault();
-            MessageCenter.publish('viewchange:DashboardList', {
-                holder: "#mainScreen > .content"
+        return false;
+    });
+    App.clearData = function() {
+        App.m.publish("clear:widgets");
+        if (App.a && App.a.widget) {
+            _.each(App.a.widgets, function (w, i) {
+                delete App.a.widgets[i];
             });
-            return false;
-        });
-        App.clearData = function() {
-            App.m.publish("clear:widgets");
-
-            if (App.a && App.a.widget) {
-                _.each(App.a.widgets, function (w, i) {
-                    delete App.a.widgets[i];
-                });
-                App.a.widgets = [];
-            };
-            App.m.publish("clear:dashboard");
-
+            App.a.widgets = [];
         };
-    }
+        App.m.publish("clear:dashboard");
+    };
+
 
 });
