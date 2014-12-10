c118437d7ff6ac246e196ed536626e9eaa173ab0
Gnibeda <anton.donnu@gmail.com>
2014-12-10 22:15:35 +0200

    added server name

--- www/js/ctrls/ServerList.js
+++ www/js/ctrls/ServerList.js
@@ -19,9 +19,13 @@ define([], function(){
             var $lst = $("#lstServers");
             for (var i = 0; i < servers.length; i++) {
                 var idx = (i+1).toString();
-                var $item = $('<li id="itemServer' + idx + '" class="table-view-cell">' + servers[i].ip + '<button id="btnEditServer' + idx + '" class="btn"><span class="icon icon-edit"></span>Edit</button><button id="btnDeleteServer' + idx + '" class="btn btn-negative" style="display:none"><span class="icon icon-trash"></span>Delete</button></li>');
+                var name = servers[i].name;
+                if (!name) name = servers[i].ip;
+                var $item = $('<li id="itemServer' + idx + '" class="table-view-cell filter-list-item">' + name + '<button id="btnEditServer' + idx + '" class="btn"><span class="icon icon-edit"></span>Edit</button><button id="btnDeleteServer' + idx + '" class="btn btn-negative" style="display:none"><span class="icon icon-trash"></span>Delete</button></li>');
                 $item.attr("serverIndex", i.toString()).appendTo($lst);
-
+                if (localStorage.currentServerId == i) {
+                    $item.addClass("active");
+                }
                 $item.off('tap').on('tap', SetCurrentServer);
                 $("#btnEditServer" + (i+1).toString()).off('tap').on('tap', EditServer);
                 $("#btnDeleteServer" + (i+1).toString()).off('tap').on('tap', DeleteServer);
@@ -30,6 +34,8 @@ define([], function(){
 
         function SetCurrentServer(e) {
             var id = $(e.currentTarget).attr("serverIndex");
+            $("#lstServers .active").removeClass("active");
+            $(e.currentTarget).addClass("active");
             localStorage.currentServerId = id;
             App.m.publish("viewchange:Login", { holder: "#loginScreen" });
         }
