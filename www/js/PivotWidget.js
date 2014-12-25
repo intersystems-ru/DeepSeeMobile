ac36bf344b7c6960b9dc86acf6c51a35a9991912
Gnibeda <anton.donnu@gmail.com>
2014-12-25 16:31:16 +0200

    light pivot support

--- www/js/PivotWidget.js
+++ www/js/PivotWidget.js
@@ -3,9 +3,15 @@ define([], function () {
         var hasDiv = false;
         var showDrilldown = false;
         this.renderWidget = function (isDrillDown) {
-            
+
+            if (this.pivot) {
+                this.pivot.dataSource.BASIC_MDX =  this.datasource.data.MDX;
+                this.pivot.refresh();
+                return;
+            }
+
             var w_selector = "#widget" + this.id || "";
-            var pivotSelector = ".pivot-container";
+            /*var pivotSelector = ".pivot-container";
             if (isDrillDown) showDrilldown = isDrillDown;
             //console.log("PW: ", this.config, w_selector);
             if (Object.keys(this.config).length < 2) return this;
@@ -14,21 +20,78 @@ define([], function () {
                 $("<div class='pivot-drilldown-container'>").hide().appendTo(w_selector);
                 $("<div class='drilldown-back-container'>").hide().appendTo(w_selector);
                 hasDiv = true;
-            }
-            if (showDrilldown) {
+            }*/
+            /*if (showDrilldown) {
                 $(w_selector).find(pivotSelector).hide();
                 pivotSelector = ".pivot-drilldown-container";
                 $(w_selector).find(pivotSelector).show();
                 $(w_selector).find(".drilldown-back-container").show();
-            }
+            }*/
             //if(!this.config.data || (this.config.data[0] === undefined)) return;
-            $(w_selector).find(pivotSelector).pivotUI(this.config.data, {
+            /*$(w_selector).find(pivotSelector).pivotUI(this.config.data, {
                 rows: this.config.rows,
                 cols: this.config.cols,
                 measures: this.config.measures,
                 onDrillDown: this.config.onDrillDown
-            });
-            if (showDrilldown) {
+            });*/
+
+            /*if (!this.pivotScroll) {
+                this.pivotScroll = new IScroll($(w_selector).parent().get(0), {vScrollbar: true});
+            }*/
+
+            var self= this;
+            self.selCellFilters = "";
+            var setup = {
+                    container: $(w_selector).get(0),
+                    dataSource: {
+                        MDX2JSONSource: App.settings.server,
+                        basicMDX: this.datasource.data.MDX,
+                        namespace: "samples",
+                        username: App.settings.username,
+                        password: App.settings.password
+                    },
+                    triggers: {
+                        drillDown: function() {
+                            $("#btnMainBack").show();
+                        },
+                        drillThrough: function() {
+                            $("#btnMainBack").show();
+                        },
+                        /* cellDrillThrough: function(d) {
+                            $(d.event.currentTarget).parent().parent().find(".selected-cell").removeClass("selected-cell");
+                            $(d.event.currentTarget).addClass("selected-cell");
+                            self.selCellFilters = d.filters;
+                            //self.pivot.tryDrillThrough(d.filters);
+                            $("#btnMainDrillthrough").show();
+                            return false;
+                        }*/
+                    },
+                    hideButtons: true,
+                    showSummary: true,
+                    triggerEvent: "click",
+                    formatNumbers: "#,###.##"
+                }
+
+           /* lp.setFilter("[DateOfSale].[Actual].[YearSold].&[2009]");
+            lp.refresh();var setup = { // Object that contain settings. Any setting may be missed.
+                    container: document.getElementById("pivot"), // HTMLElement on DOM which will contain table.
+                    dataSource: {
+                        MDX2JSONSource: "http://localhost:57772/SAMPLES", // MDX2JSON source server address
+                        basicMDX: "SELECT NON EMPTY [Product].[P1].[Product Category].Members ON 0, NON EMPTY [Outlet].[H1].[Region].Members ON 1 FROM [HoleFoods]" // basic MDX which are going to be rendered when widget loads
+                    }
+                    , caption: "My table" // if set, table basic caption will be replaced by this text
+                    , showSummary: true // show summary by columns
+                    , formatNumbers: "#,###.##" // number formatting mask // @deprecated
+                    , drillDownTarget: "dashboard name.dashboard" // custom drilldown target, DeepSee only.
+                },*/
+
+            this.pivot = new LightPivotTable(setup);
+
+            //new IScroll($(w_selector).parent().parent().parent().get(0));
+            //lp.setFilter("[DateOfSale].[Actual].[YearSold].&[2009]");
+            //lp.refresh();
+
+            /*if (showDrilldown) {
                 $(w_selector).find(".drilldown-back-container").text("<").one("tap", function () {
                     //console.log("PIVOT SELECTOR", pivotSelector);
                     $(w_selector).find(".pivot-drilldown-container").hide();
@@ -37,7 +100,7 @@ define([], function () {
                     $(w_selector).find(".pivot-container").show();
                     showDrilldown = false;
                 });
-            }
+            }*/
         }
     }
     PivotWidget.prototype.toString = function () {
