497c70b1d012d4ea8098bf576fa84fc8a076ccfc
Gnibeda <anton.donnu@gmail.com>
2015-01-14 22:41:55 +0200

    controls support added

--- www/js/main.js
+++ www/js/main.js
@@ -31,6 +31,10 @@ require([
         title: "InterSystems DeepSee™"
     };
 
+    App.ui = {
+        navBar: $("#mainScreen .bar-nav")
+    };
+
     App.m = MessageCenter;
     App.v = ViewManager;
     App.filters = [];
@@ -39,6 +43,27 @@ require([
         callback: function (d) {
             for (var i = 0; i < d.data.data.length; i++) {
                 App.filters.push(new Filter(d.data.data[i], d.data.cube, d.data.widget));
+            }
+            if (d.data.widget.controls) if (d.data.widget.controls.length != 0) {
+                // move controls to filters if exists
+                for (var c = 0; c < d.data.widget.controls.length; c++) {
+                    for (var i = 0; i < App.filters.length; i++) if (App.filters[i].widget == d.data.widget) {
+                        if (App.filters[i].path == d.data.widget.controls[c].targetProperty) {
+                            var val = d.data.widget.controls[c].value;
+                            var disp = d.data.widget.controls[c].targetPropertyDisplay;
+                            var matches = val.match(/\[(.*?)\]/);
+                            if (matches) disp = matches[1];
+                            d.data.widget.filters.setFilter({
+                                name: App.filters[i].name,
+                                path: App.filters[i].path,
+                                value: [val],
+                                valueName: [disp]
+                            }, true);
+                        }
+                    }
+                }
+                if (d.data.widget == App.a.widgets[App.a.activeWidget]) $("#btnMainFilter").addClass("tab-item-green");
+                d.data.widget.controls = [];
             }
         }
     });
@@ -50,6 +75,7 @@ require([
 
     $("a.nav-home").off('tap').on('tap', function (e) {
         e.preventDefault();
+        //App.clearData();
         MessageCenter.publish('viewchange:DashboardList', {
             holder: "#mainScreen > .content"
         });
