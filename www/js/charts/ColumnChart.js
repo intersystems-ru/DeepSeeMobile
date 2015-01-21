1e42a95d0fc9bdf8c4bcfa87436d2b107a8c2a40
Gnibeda <anton.donnu@gmail.com>
2015-01-21 21:41:53 +0200

    drilldown support

--- www/js/charts/ColumnChart.js
+++ www/js/charts/ColumnChart.js
@@ -1,7 +1,10 @@
 define(['charts/ChartBase'], function (cb) {
-    return {
+    var o = {
         type: "highcharts",
             callback: function (d) {
+                this.config.storedCats = [];
+                this.config.c = null;
+                this.config.p = null;
                     var data = d.data;
                     this.config.xAxis.title = {
                         text: data.Cols[1].caption
@@ -24,30 +27,84 @@ define(['charts/ChartBase'], function (cb) {
                     this.config.series = [{
                         colorByPoint: true,
                         data: data.Data,
-                        name: data.Cols[0].tuples[0].caption
+                        name: data.Cols[0].tuples[0].caption,
+                        format: data.Cols[0].tuples[0].format
                     }];
-
-
         //this.renderWidget();
             },
         config: {
+
+            storedCats: [],
+            requestDrilldown: function(point) {
+
+                var mc = require("MessageCenter");
+                this.p = point;
+                this.c = point.series.chart;
+
+                mc.subscribe("data_acquired:drilldown", {
+                    subscriber: this,
+                    once: true,
+                    callback: function (d) {
+                        if (!d.data.Data) return;
+                        if (d.data.Data.length == 0) return;
+
+                        var data = d.data;
+                        this.storedCats.push(this.c.xAxis[0].categories);
+                        this.c.xAxis[0].categories = [];
+                        for (var i = 0; i < data.Cols[1].tuples.length; i++) {
+                            this.c.xAxis[0].categories.push(data.Cols[1].tuples[i].caption.toString());
+                            data.Data[i] = {
+                                y: data.Data[i],
+                                drilldown: true,
+                                cube: data.Info.cubeName,
+                                path: data.Cols[1].tuples[i].path
+                            };
+                        };
+
+                        this.c.addSeriesAsDrilldown(this.p,{
+                            colorByPoint: true,
+                            data: data.Data,
+                            name: data.Cols[0].tuples[0].caption,
+                            format: data.Cols[0].tuples[0].format
+                        });
+                    }
+                });
+
+                var self = this;
+                mc.publish("data_requested:drilldown", {
+                    cubeName: self.p.cube,
+                    path: self.p.path
+                });
+                mc = null;
+            },
+            tooltip: {
+                formatter: cb.defaultTooltipFormatter
+            },
+            drilldown: {
+                drillUpButton: {
+                    relativeTo: 'spacingBox',
+                    position: {
+                        y: 0,
+                        x: 0
+                    },
+                    theme: {
+                        fill: "#DCE7F1"
+                    }
+                },
+                series: []
+            },
             chart: {
                 type: 'column',
-                    margin: 75,
-                /*options3d: {
-                 enabled: true,
-                 alpha: 15,
-                 beta: 15,
-                 viewDistance: 25,
-                 depth: 40
-                 }*/
+                margin: 75,
+                events: {
+                    drillup: function (e) {
+                        this.xAxis[0].categories = o.config.storedCats.pop();
+                    }
+                }
             },
             title: {
                 text: ''
             },
-            /*subtitle: {
-             text: 'Notice the 3D'
-             },*/
             legend:{
                 enabled:false
             },
@@ -56,16 +113,40 @@ define(['charts/ChartBase'], function (cb) {
                     depth: 25,
                     dataLabels: {
                         enabled: false
+                    },
+                    point: {
+                        events: {
+                            click: function (e) {
+                                var p = e.point;
+                                if (p) {
+                                    var chart = p.series.chart;
+                                    if (chart.__selPoint == p) {
+                                        o.config.requestDrilldown(p);
+                                    } else {
+                                        chart.__selPoint = p;
+                                    }
+                                }
+                            }
+                        }
                     }
                 }
             },
             xAxis: {
-                categories: []
+                categories: [],
+                labels: {
+                    rotation: -45
+                }
             },
             yAxis: {
                 opposite: true
             },
-            series: []
+            series: [],
+
+            lang: {
+                drillUpText: '< Back'
+            }
         }
     }
+
+    return o;
 });
