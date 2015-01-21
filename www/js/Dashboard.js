19face72151ff260b43d4f16d378e13adc0befe5
Gnibeda <anton.donnu@gmail.com>
2015-01-21 21:43:31 +0200

    clean up and some fixes

--- www/js/Dashboard.js
+++ www/js/Dashboard.js
@@ -62,6 +62,7 @@ define([
             var requestedFiltersForCube = [];
             var widgets = d.children;
             var self = this;
+
             for (var i = 0; i < widgets.length; i++) {
 
                 var widget = widgets[i];
@@ -76,9 +77,6 @@ define([
                     }
                 };
 
-                if (widget.type == "pivot") {
-                    widget_config.datasource.data.MDX = widget.basemdx;
-                }
                 widget_config.filters = [];
                 _.each(widget.children, function (filter) {
                     widget_config.filters.push({
@@ -103,6 +101,8 @@ define([
                         self.widgets[self.widgets.length - 1].controls.push(widgets[i].controls[k]);
                     }
                 }
+
+                $('<div class="circle"></div>').appendTo(".markets");
 
                 widget = null;
                 widget_config = null;
@@ -140,24 +140,17 @@ define([
     };
 
     Dashboard.prototype.hideMarkers = function() {
-        $(".tri-left").hide();
-        $(".tri-right").hide();
+        $(".markets").empty();
     };
 
     Dashboard.prototype.updateMarkers = function() {
-        $(".tri-left").hide();
-        $(".tri-right").hide();
-        if (App.a.widgets.length == 0) return;
-        if (App.a.activeWidget == 0) {
-            $(".tri-right").show();
+        if (App.a.widgets.length <= 1) {
+            this.hideMarkers();
             return;
         }
-        if (App.a.activeWidget == App.a.widgets.length - 1) {
-            $(".tri-left").show();
-            return;
-        }
-        $(".tri-right").show();
-        $(".tri-left").show();
+
+        $(".markets").find(".circle").removeClass("circle-full");
+        $(".markets").find(".circle:eq("+App.a.activeWidget+")").addClass("circle-full");
     }
 
     /**
@@ -168,67 +161,7 @@ define([
     Dashboard.prototype.toString = function () {
         return "Dashboard";
     };
-/**
-         * Renders up the whole dashboard with its widgets and so on.
-         * @function module:Dashboard#addWidget
-         * @param config {Object}  Configuration object for Widget
-         * @example
-         *  a.addWidget({
-        title: "Очередь пациентов по профилям",
-        amconfig: {
-            "type": "serial",
-            "pathToImages": "http://cdn.amcharts.com/lib/3/images/",
-            "categoryField": "category",
-            "rotate": true,
-            "startDuration": 1,
-            "categoryAxis": {
-                "autoRotateCount": -5,
-                "gridPosition": "start",
-                "inside": true
-            },
-            "trendLines": [],
-            "graphs": [
-                {
-                    "balloonText": "[[title]] of [[category]]:[[value]]",
-                    "fillAlphas": 1,
-                    "id": "AmGraph-1",
-                    "title": "",
-                    "type": "column",
-                    "valueField": "value"
-      }
-     ],
-            "guides": [],
-            "valueAxes": [
-                {
-                    "axisTitleOffset": -7,
-                    "id": "ValueAxis-1",
-                    "title": "Кол-во человек"
-      }
-     ],
-            "allLabels": [],
-            "balloon": {},
-            "titles": [
-                {
-                    "id": "Title-1",
-                    "size": 15,
-                    "text": ""
-      }
-     ],
 
-        },
-        datasource: {
-            data: {
-                MDX: 'SELECT NON EMPTY {TOPPERCENT(ORDER([ProfileMODep].[H1].[Profile].Members,Measures.[%COUNT],BDESC),80),%LABEL(SUM(BOTTOMPERCENT(ORDER([ProfileMODep].[H1].[Profile].Members,Measures.[%COUNT],BDESC),20)),"Другой",,,,"font-style:italic;")} ON 1 FROM [QueueCube]'
-            }
-        },
-        filters: [{
-            name: "status",
-            path: "[status].[H1].[status]",
-            value: "&[0]",
-            valueName: "0"
-        }]
-    });
-         */
     Dashboard.prototype.addWidget = function (config) {
             var self = this;
             var def = $.Deferred();
@@ -246,37 +179,12 @@ define([
                 //mc.publish("set_active_widget", { id: 0 });
             }
 
-        /** TODO: make widget page change from popover
-            var name = widget.name;
-            if (!name) name = "Widget #" + this.widgets.length.toString();
-            var el = $("<li num="+this.widgets.length+" class='table-view-cell'>" + name + "</li>");
-            el.on("tap", function(e) {
-                var slide = $(self.config.holder).find(".slide").get(0);
-                var offset = (parseInt($(e.target).attr("num"))-1) * slide.offsetWidth;
-                // Move slide
-                var slider = $(self.config.holder).find('.slide-group').get(0);
-                slider.style['-webkit-transition-duration'] = '.2s';
-                slider.style.webkitTransform = 'translate3d(-' + offset + 'px,0,0)';
-                $("#widgetList").removeClass("visible");
-                $('div.backdrop').remove();
-            });
-            $("#widgetList").find(".table-view").append(el);
-**/
             widget = null;
             return def.promise();
         };
 
         Dashboard.prototype.createHolder = function(){
             var holder = (this.config && this.config.holder) ? this.config.holder : "mainScreen > .content";
-
-
-            //temp//
-            /*$(holder + " > *").remove();
-            var tbl = $("<table border='1' width='100%'></table>");
-            tbl.appendTo(holder);
-            return;*/
-            ///////////
-
                 require(['text!../views/Dashboard.html'], function (html) {
                     $(holder + " > *").remove();
                     $(holder).append(html);
@@ -314,39 +222,8 @@ define([
                     });
                 }
             });
-
-           // setTimeout(this.tileView, 1000);
-
-
             return this;
         };
-
-        Dashboard.prototype.tileView = function() {
-            var slide = $(App.a.config.holder).find('.slide-group');
-            var tbl = $("<table border='1'></table>");
-            var tr = null;
-            var td = null;
-            var c = 0;
-            slide.find(".slide").each(function(n, e){
-                if (c == 0) {
-                    tr = $("<tr></tr>");
-                }
-                tr.appendTo(tbl);
-                td = $("<td></td>");
-                $(e).find("div:eq(0)").appendTo(td);
-                td.appendTo(tr);
-                c++;
-                if (c == 2) c = 0;
-                console.log(e);
-            });
-
-            tbl.css("width", "300px")
-                .css("border", "1px solid")
-                .appendTo(App.a.config.holder);
-
-            $(App.a.config.holder).find(".slider").remove();
-        }
-
     return Dashboard;
 
 
