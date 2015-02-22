438cccc8a3910f2aa6ffbf90722de649af09bc56
Gnibeda <anton.donnu@gmail.com>
2015-02-22 23:28:24 +0200

    folder support

--- www/js/ctrls/DashboardList.js
+++ www/js/ctrls/DashboardList.js
@@ -1,7 +1,34 @@
-define([/*'lib/iscroll-probe',*/ 'MessageCenter', 'Dashboard'], function (/*_iscroll, */MessageCenter, Dashboard) {
+define(['Language', 'MessageCenter', 'Dashboard'], function (Lang, MessageCenter, Dashboard) {
     function DashboardListController() {
+        var self = this;
+        this.FilterFolders = function(childs) {
+            var items = [];
+            var deep = App.folder.split("/").length;
+            for (var i = 0; i < childs.length; i++) {
+                var p = childs[i].path.split("/");
+                var path = "";
+                for (var k = 0; k < deep; k++) path += p[k] + "/";
+                if (path != "") path = path.substr(0, path.length - 1);
+                path = path.toLowerCase();
+                if (path == App.folder.toLowerCase()) {
+                    var firstPart = childs[i].path.toLowerCase().replace(App.folder.toLowerCase() + "/", "").split("/");
+                    items.push({path: firstPart[0], desc: firstPart[0], title: childs[i].title, isFolder: firstPart.length > 1, icon: "fa-dashboard"});
+                    if (items[items.length - 1].isFolder) {
+                        items[items.length - 1].icon = "fa-folder-o";
+                        items[items.length - 1].title = items[items.length - 1].path;
+                        items[items.length - 1].desc = Lang.getText("folder");
+                    }
+                }
+            }
+            return items.sort(function(a, b){
+                if (a.isFolder && !b.isFolder) return -1;
+                if (!a.isFolder && b.isFolder) return 1;
+                if (a.title > b.title) return 1; else return -1;
+            });
+        }
 
         var onDashboardListAcquired = function (e) {
+            var items = self.FilterFolders(e.children);
             $("#btnMainRefresh").off('tap').on('tap', function() {
                 if (App.a) {
                     if (App.a.widgets[App.a.activeWidget]) App.a.widgets[App.a.activeWidget].requestData();
@@ -11,7 +38,7 @@ define([/*'lib/iscroll-probe',*/ 'MessageCenter', 'Dashboard'], function (/*_isc
                 }
             });
 
-            App.dashboardList = e.children;
+            App.dashboardList = items;
             if (sessionStorage.getItem("dashboard_list") == null) {
                 sessionStorage.setItem('dashboard_list', JSON.stringify(e))
             };
@@ -20,15 +47,23 @@ define([/*'lib/iscroll-probe',*/ 'MessageCenter', 'Dashboard'], function (/*_isc
                 $("#btnMainFilter").hide();
                 var template = Handlebars.compile(html);
                 var rendered = template({
-                    dashboardList: e.children
+                    dashboardList: items
                 });
                 var final = $(rendered);
                 final.show();
                 final.find("li").off("tap").on('tap', function (e) {
+                    var item = App.dashboardList[$(this).data('id')];
                     e.preventDefault();
-                    final.find("> *").removeClass("active");
-                    $(this).addClass("active");
-                    App.a = new Dashboard(App.dashboardList[$(this).data('id')].path).render();
+                    if (item.isFolder) {
+                        App.folder += "/" + item.path;
+                        App.m.publish("viewchange:DashboardList", {holder: "#mainScreen > .content"});
+                        $("#btnMainBack").show();
+                    } else {
+                        $("#btnMainBack").hide();
+                        final.find("> *").removeClass("active");
+                        $(this).addClass("active");
+                        App.a = new Dashboard(App.folder + "/" + item.path).render();
+                    }
                     return false;
                 });
 
